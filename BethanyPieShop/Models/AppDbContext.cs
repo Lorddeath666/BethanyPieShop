c6cc42b927b65c6702612d966abb68ba5362a362
karololszewski1989@gmail.com <karololszewski1989@gmail.com>
2019-07-05 11:50:15 +0200

    Add form for feedback and validation for this form

--- BethanyPieShop/Models/AppDbContext.cs
+++ BethanyPieShop/Models/AppDbContext.cs
@@ -1,8 +1,4 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BethanyPieShop.Models
 {
@@ -14,5 +10,6 @@ namespace BethanyPieShop.Models
         }
 
         public DbSet<Pie> Pies { get; set; }
+        public DbSet<Feedback> Feedbacks { get; set; }
     }
 }
